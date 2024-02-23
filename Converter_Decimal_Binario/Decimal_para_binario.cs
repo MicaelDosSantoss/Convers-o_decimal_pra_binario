80c7fa413485b8ee77423b4fe41f306e0ba49eca
MicaelDosSantos <micaelds243@gmail.com>
2024-02-23 13:01:11 -0300

    Conversão

---
+++ Converter_Decimal_Binario/Decimal_para_binario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_Decimal_Binario
+{
+    internal class Decimal_para_binario
+    {
+        public int valor;
+        private int arm;
+        private int conta;
+        private string valor_convert;
+
+        public string Convert(int value)
+        {
+            arm = value % 2;
+            Console.WriteLine("valor da conta: " + value + " Resto: " + arm);
+
+            valor_convert = arm.ToString();
+
+            do {
+                conta = value / 2;
+                arm = conta % 2;
+                Console.WriteLine("valor da conta: "+conta+" Resto: "+ arm);
+                valor_convert += arm.ToString();
+                value = conta;
+            } while (conta / 2 != 0);
+
+            return valor_convert;
+        }
+
+    }
+}
---
+++ Converter_Decimal_Binario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_Decimal_Binario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Decimal_para_binario d = new Decimal_para_binario();
+            string valorFinal = (d.Convert(47));
+
+            Console.WriteLine(valorFinal.Reverse().ToArray());
+
+        }
+    }
+}
